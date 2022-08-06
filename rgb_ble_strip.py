c6967b0cb86332a605994ab834b59a0e9fddc823
dvgniele <ktm17@live.it>
2022-08-06 19:34:31 +0200

    feat: added bluetooth low energy feature. fully working

--- rgb_ble_strip.py
+++ rgb_ble_strip.py
@@ -1,14 +1,19 @@
 import sys
 from functools import cached_property
+from dataclasses import dataclass
 
 import asyncio
 import qasync
 
-from PyQt5 import QtGui
 from PyQt5.QtCore import QObject, QRect, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QComboBox, QColorDialog
 
+from bleak.backends.device import BLEDevice
+from bleak import BleakClient, BleakScanner
+
 rgb = {'red': 255, 'green': 255, 'blue': 255}
+autoscan = True
+autoconnect = True
 
 
 class Window(QMainWindow):
@@ -49,7 +54,8 @@ class Window(QMainWindow):
         self._connect_btn.clicked.connect(self.ble_connect)
         self._color_btn.clicked.connect(self.ble_send)
 
-        self.ble_scan()
+        if autoscan:
+            self.ble_scan()
 
     @cached_property
     def devices(self):
@@ -59,21 +65,59 @@ class Window(QMainWindow):
     def client(self):
         return self._client
 
+    async def create_client(self, device):
+        if self._client:
+            await self._client.stop()
+
+        self._client = QBleakClient(device)
+        self._client.msg.connect(self._client.msg)
+        await self._client.start()
+
     @qasync.asyncSlot()
     async def ble_scan(self):
-        pass
+        self.devices.clear()
+        self._devices_cb.clear()
+
+        self._label.setText('Scanning')
+        self._label.setStyleSheet("QLabel {color: orange;}")
+
+        devices = await BleakScanner.discover()
+        self.devices.extend(devices)
+        for i, device in enumerate(self.devices):
+            if str(device.name).startswith('QHM'):
+                self._devices_cb.insertItem(i, device.name, device)
+
+        if len(devices) > 0:
+            self._devices_cb.setCurrentIndex(0)
+
+            self._label.setText('Found!')
+            self._label.setStyleSheet("QLabel {color: green;}")
+        else:
+            self._label.setText('No devices!')
+            self._label.setStyleSheet("QLabel {color: red;}")
+
+        if len(devices) == 1 and autoconnect:
+            self.ble_connect()
 
     @qasync.asyncSlot()
     async def ble_connect(self):
-        pass
+        self._label.setText('Connecting')
+        self._label.setStyleSheet("QLabel {color: orange;}")
+
+        device = self._devices_cb.currentData()
+        if isinstance(device, BLEDevice):
+            await self.create_client(device)
+            self._label.setText('Connected')
+            self._label.setStyleSheet("QLabel {color: green;}")
+            self._scan_btn.setEnabled(False)
+            self._connect_btn.setEnabled(False)
 
     @qasync.asyncSlot()
     async def ble_send(self):
         global rgb
 
-        print(rgb)
-        rgb = ColorSelector().get_color()
-        print(rgb)
+        rgb = ColorSelector().color()
+        await self.client.write()
 
 
 class ColorSelector(QMainWindow):
@@ -94,8 +138,60 @@ class ColorSelector(QMainWindow):
 
         return rgb
 
-    def get_color(self):
+    def color(self):
         return rgb
+
+
+@dataclass
+class QBleakClient(QObject):
+    device: BLEDevice
+    msg = pyqtSignal(bytes)
+
+    def __post_init__(self):
+        super().__init__()
+
+        self.UART_SERVICE_UUID = None
+        self.UART_RX_CHAR_UUID = None
+        self.UART_TX_CHAR_UUID = None
+        self.UART_SAFE_SIZE = 20
+
+    @cached_property
+    def client(self) -> BleakClient:
+        return BleakClient(self.device, disconnected_callback=self.disconnect)
+
+    async def start(self):
+        await self.client.connect()
+        services = await self.client.get_services()
+
+        for item in services.characteristics:
+            if services.characteristics[item].properties[0] == 'write-without-response' and not self.UART_TX_CHAR_UUID:
+                self.UART_TX_CHAR_UUID = services.characteristics[item].uuid
+            elif services.characteristics[item].properties[0] == 'read' and not self.UART_RX_CHAR_UUID:
+                self.UART_RX_CHAR_UUID = services.characteristics[item].uuid
+
+    async def stop(self):
+        await self.client.disconnect()
+
+    async def write(self):
+        lst = [86, rgb['red'], rgb['green'], rgb['blue'],
+               (int(10*255/100) & 0xFF), 256-16, 256-86]
+
+        values = bytearray(lst)
+
+        try:
+            await self.client.write_gatt_char(self.UART_TX_CHAR_UUID, values, False)
+        except Exception as e:
+            print(e)
+
+    def disconnect(self):
+        for task in asyncio.all_tasks():
+            task.cancel()
+
+        print('Device disconnected.')
+
+    def read(self, data: bytearray):
+        self.msg.emit(data)
+        print('received: ', data)
 
 
 def main():
